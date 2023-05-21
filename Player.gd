b0f06b7a1c213a5e4be30de40851d2298757713a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-05-21 13:33:06 +0200

    Reset the scene if the player falls in the void

--- Player.gd
+++ Player.gd
@@ -13,6 +13,8 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		if global_position.y < -3:
+			get_tree().change_scene_to_file("res://game.tscn")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
