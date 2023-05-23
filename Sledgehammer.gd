f3e418c2af53bf81515d69af1308598661ce7409
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-05-23 18:34:03 +0200

    Show dealt damage on the screen

--- Sledgehammer.gd
+++ Sledgehammer.gd
@@ -1,8 +1,17 @@
 extends Node3D
 
+@onready var damage_label: Label = $"../../../UI/DamageLabel"
+
 func _on_body_entered(body):
 	if (body.name == "Enemy"):
-		body.damage(get_damage())
+		damage(body)
+
+func damage(enemy):
+	var dmg = get_damage()
+	damage_label.text = "%0.1f" % dmg
+	enemy.damage(dmg)
+
+
 
 var old_global_position: Vector3
 
@@ -10,5 +19,5 @@ func _physics_process(delta):
 	old_global_position = global_position
 
 func get_damage():
-	var velocity = global_position - old_global_position
-	return 100 * velocity.length()
+	var velocity = (global_position - old_global_position) * 60
+	return velocity.length()
