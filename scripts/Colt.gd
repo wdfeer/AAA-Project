dc9783c3b2803f480af162b036fade7be134cf80
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-09-27 08:16:37 +0200

    Decrease Colt damage

--- scripts/Colt.gd
+++ scripts/Colt.gd
@@ -3,8 +3,8 @@ extends Weapon
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var audio_player: AudioStreamPlayer3D = $Sound
 
-const attack_distance = 1000
-const dmg = 60
+const ATTACK_DISTANCE = 1000
+const DMG = 40
 
 func get_attack_cooldown():
 	return 1
@@ -13,12 +13,12 @@ func attack():
 	var space_state = get_world_3d().direct_space_state
 	
 	var origin: Vector3 = get_parent_node_3d().global_position
-	var destination: Vector3 = origin - get_parent_node_3d().global_transform.basis.z * attack_distance
+	var destination: Vector3 = origin - get_parent_node_3d().global_transform.basis.z * ATTACK_DISTANCE
 	
 	var query = PhysicsRayQueryParameters3D.create(origin, destination, player.collision_mask)
 	var result = space_state.intersect_ray(query)
 	if result and result.collider is EnemyBase:
-		damage(result.collider, dmg)
+		damage(result.collider, DMG)
 	
 	play_fire_animation()
 	play_fire_sound()
