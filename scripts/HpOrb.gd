d5f2cf01637c1b7a12030041d17628b354a1aa81
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-09-27 07:11:04 +0200

    Add slight up-down HpOrb animation

--- scripts/HpOrb.gd
+++ scripts/HpOrb.gd
@@ -2,8 +2,11 @@ extends Area3D
 
 const HEAL_AMOUNT: float = 5
 
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+
 func _ready():
 	body_entered.connect(on_body_entered)
+	anim_player.play("OrbUpDown")
 
 func on_body_entered(body: Node3D):
 	if body is Player:
